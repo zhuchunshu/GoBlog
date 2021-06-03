22c04904f2576fe2a7d2e2eb83cb4f0e7ec8097c
zhuchunshu <laravel@88.com>
2021-06-03 22:53:51 +0800

    更新: 创建路由文件

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/zhuchunshu/GoBlog/routes"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app := fiber.New(fiber.Config{
+		ServerHeader:  "FecApi",
 	})
+	app.Use(logger.New())
 
-	log.Fatal(app.Listen(":3000"))
+	// 初始化数据库
+	//initDatabase()
+
+	// 注册路由
+	routes.InitRouters(app)
+
+	// 设置端口
+	//Serverport := helpers.JsonDecode(ServerConfig, "port")
+	server := app.Listen(":3000")
+	log.Fatal(server)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func InitRouters(app *fiber.App) {
+	//RegisterWebRoutes(app)
+	//api.InitRouters(app)
+}
